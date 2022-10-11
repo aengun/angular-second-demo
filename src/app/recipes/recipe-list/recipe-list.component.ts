e0860a40b3b632f3bbc343fe05d4c21109daa0df
aengun <aengun@icloud.com>
2022-10-11 12:21:50 +0900

    레시피 디테일 디스플레이 하기

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe1', 'This is simply a test', 'https://perfectdailygrind.com/wp-content/uploads/2019/01/unnamed-2-3.jpg')
+    , new Recipe('A Test Recipe2', 'This is simply a test', 'https://perfectdailygrind.com/wp-content/uploads/2019/01/unnamed-2-3.jpg')
+    , new Recipe('A Test Recipe3', 'This is simply a test', 'https://perfectdailygrind.com/wp-content/uploads/2019/01/unnamed-2-3.jpg')
+    , new Recipe('A Test Recipe4', 'This is simply a test', 'https://perfectdailygrind.com/wp-content/uploads/2019/01/unnamed-2-3.jpg')
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
